8d2a09813d98d088a6c5d23c976eb3e92e90c9b8
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-07-29 09:12:34 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	println(findMin(5144, 95445, 4535, 52423, 5234))
+
+}
+
+func findMin(numbers ...int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	min := numbers[0]
+
+	for _, i := range numbers {
+		if i < min {
+			min = i
+		}
+	}
+	return min
+}

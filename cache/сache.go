62fe70c6a5c582a1f1eebc3332456c90c61de353
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-02 16:58:09 +0100

    add mutex in func

--- cache/сache.go
+++ cache/сache.go
@@ -1,19 +1,23 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // Cache is a basic in-memory storage for data.
 type Cache struct {
 	data map[string]cacheObject
+	mu   sync.RWMutex // Mutex to ensure thread-safety
 }
 
-// cacheObject struct to store value and expired in Cache.
+// cacheObject struct to store value and expiration in Cache.
 type cacheObject struct {
 	value   interface{}
 	expired int64
 }
 
-// New creates new Cache.
+// New creates and returns a new Cache.
 func New() *Cache {
 	// Create a variable result
 	result := &Cache{}
@@ -26,6 +30,10 @@ func New() *Cache {
 
 // Set adds a value.
 func (c *Cache) Set(key string, value interface{}, ttl time.Duration) error {
+	// Lock for writing
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	err := validateKey(key)
 	if err != nil {
 		return err
@@ -40,17 +48,29 @@ func (c *Cache) Set(key string, value interface{}, ttl time.Duration) error {
 
 // Get retrieves a value.
 func (c *Cache) Get(key string) (interface{}, error) {
+	// Lock for reading
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	err := validateKey(key)
 	if err != nil {
 		return nil, err
 	}
-	result := c.data[key]
 
-	return result, nil
+	result, exists := c.data[key]
+	if !exists || isExpired(result) {
+		return nil, nil // or a specific error indicating the key does not exist or is expired
+	}
+
+	return result.value, nil
 }
 
 // Delete removes a value.
 func (c *Cache) Delete(key string) error {
+	// Lock for writing
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	err := validateKey(key)
 	if err != nil {
 		return err
@@ -61,16 +81,24 @@ func (c *Cache) Delete(key string) error {
 
 // Exists checks if a key exists.
 func (c *Cache) Exists(key string) bool {
+	// Lock for reading
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	err := validateKey(key)
 	if err != nil {
 		return false
 	}
-	_, result := c.data[key]
-	return result
+	_, exists := c.data[key]
+	return exists
 }
 
 // Keys returns a list of keys.
 func (c *Cache) Keys() ([]string, error) {
+	// Lock for reading
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	result := make([]string, 0, len(c.data))
 	for key := range c.data {
 		result = append(result, key)

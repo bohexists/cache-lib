8bd0ce38f9fd51651eb37f470269c24a488e4578
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-02 21:33:04 +0100

    add mutex on ttl

--- cache/ttl.go
+++ cache/ttl.go
@@ -6,11 +6,13 @@ import "time"
 func (c *Cache) LaunchCleaner(interval time.Duration) {
 	go func() {
 		for range time.Tick(interval) {
+			c.mu.Lock() // Заблокировать доступ для записи
 			for key, object := range c.data {
 				if isExpired(object) {
 					delete(c.data, key)
 				}
 			}
+			c.mu.Unlock() // Разблокировать доступ
 		}
 	}()
 }
